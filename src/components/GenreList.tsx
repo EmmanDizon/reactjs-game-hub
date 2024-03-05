736f6b768f5069bd8b8e3c732f86c72dfe2e43d5
Emman Jay Dizon <emmanjhay@gmail.com>
2024-03-05 20:31:41 +0800

    remove spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,7 +5,6 @@ import {
   Image,
   List,
   ListItem,
-  Spinner,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -16,9 +15,8 @@ interface Props {
 }
 
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
-  const { data, isLoading } = useGenres();
+  const { data } = useGenres();
 
-  if (isLoading) return <Spinner />;
   return (
     <>
       <Heading fontSize="2xl" marginBottom={3}>

ea0c06225c24d29649f6284585804a88e0f399f9
Emman Jay Dizon <emmanjhay@gmail.com>
2024-03-08 17:31:15 +0800

    game details feature

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,7 +6,9 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
+  description_raw: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
